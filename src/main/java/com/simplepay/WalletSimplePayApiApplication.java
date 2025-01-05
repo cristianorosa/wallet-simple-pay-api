09782065830dee04fbe8ba65017fed57b371a959
Cristiano Rosa <cristiano@inverter.com.br>
2025-01-05 12:22:24 -0300

    feat: First commit.

---
+++ src/main/java/com/simplepay/WalletSimplePayApiApplication.java
@@ -0,0 +1,13 @@
+package com.simplepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSimplePayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletSimplePayApiApplication.class, args);
+	}
+
+}
